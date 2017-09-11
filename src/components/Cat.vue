38b20416bb52de9970572e0d15412b7309d0eccf
A178281 <desteban@externos.bankia.com>
2017-09-11 10:11:23 +0200

    phase 1 - click on cat

---
+++ src/components/Cat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cat">
+    <img v-on:click="counter += 1" src="http://static.vix.com/es/sites/default/files/styles/large/public/imj/3/30-cosas-de-los-gatos-que-no-sabias-3.jpg?itok=kG-1h5_b">
+    <h1>Count: {{counter}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cat',
+    data () {
+      return {
+        counter: 0
+      }
+    }
+  }
+</script>
